8b4b1a1024c54322f4813b9cced7ce72a98ea8c5
ShneiderOrtiz <shneiderortiz20@gmail.com>
2025-03-06 21:24:57 -0500

    Add files via upload

---
+++ Ejercicio 2.py
@@ -0,0 +1,8 @@
+#Entrada
+base=float(input("Ingrese la base del rectángulo"))
+altura=float(input("Ingrese la Altura del rectángulo"))
+
+#Caja negra
+
+Area=(base * altura)
+print(f"El área del rectángulo es", Area)
